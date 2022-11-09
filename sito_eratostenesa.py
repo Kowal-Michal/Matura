1903c2baeecce0971aa2dfdbb686602762774cfb
Kowal-Michal <kowalm04d@lo5.bielsko.pl>
2022-11-09 23:40:47 +0100

    create a program that realize the sieve of Eratosthenes, where n is max number in a set

---
+++ sito_eratostenesa.py
@@ -0,0 +1,17 @@
+import math
+def sito(n):
+    tab=[]
+    liczby_pierwsze=[]
+    liczby_pierwsze=[True]*(n+1)
+    for i in range(2,int(math.sqrt(n))+1):
+         if liczby_pierwsze[i]:
+             for j in range(2*i,n+1,i):
+                 liczby_pierwsze[j]=False
+    for i in range(0,n+1):
+        if liczby_pierwsze[i]==True:
+            tab.append(i)
+        else:
+            continue
+    print(tab)
+n=int(input('Podaj ile liczb pierwszych chcesz wyznaczyć: '))
+sito(n)
