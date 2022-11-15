60e8f77f05e3d2c901bc98e3db9092b7e0ce2dde
Kowal-Michal <kowalm04d@lo5.bielsko.pl>
2022-11-15 18:25:15 +0100

    Create a program that realize leibnitz formula calculating pi using specific epsilon

--- leibnitz_pi.py
+++ leibnitz_pi.py
@@ -1,13 +1,15 @@
 
 def leibnitz(eps):
-    p1=0
-    p2=0
+    pi=4
+    x=0
     i=1
-    while abs(p2-p1)<=eps:
-        p1=(-1)**i/2*i+1
-        p2=(-1)**(i-1)/2*(i-1)+1
+    while abs(x-pi)>=eps:
+        x=pi
+        if i%2==0:
+            pi=pi+4/(2*i+1)
+        else:
+            pi=pi-4/(2*i+1)
         i+=1
-    return p1*4
+    return pi
 eps=float(input('Podaj epsilon: '))
 print(leibnitz(eps))
-
